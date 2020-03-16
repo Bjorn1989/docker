01916601f3c82e1a3b30f005e53108fff604d2ea
Bjorn Tosoc <bjorntosoc@Bjorns-MacBook-Pro.local>
2020-03-15 18:24:57 -0700

    New repo

---
+++ Readme.md
@@ -0,0 +1,4 @@
+ReadMe
+
+1. This is a test readme file.
+2.
---
+++ test.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#sample for tesing script
+#MY_SHELL=cutess
+#echo "i am $MY_SHELL"
+#if [ "$MY_SHELL" = "cute" ]
+#then
+#echo "the value if variable is the same"
+#elif [ "$MY_SHELL" = "cutes" ]
+#then
+#echo "the value of variable is the same1"
+#else
+#echo "the value is not either cute or cutes" 
+
+#fi
+
+#sample for loop
+#FILESSS=$(ls)
+
+#for FILESSS in $FILESSS
+#do
+#echo "One by one listing of $FILESSS "
+
+#done
+
+#using read syntax
+#read -p "Please log in:" USER
+#echo "Verifying account: $USER"
+
+#HOST="go34gle.com"
+#ping -c 1 $HOST
+#if [ $? -ne "0" ]
+#then 
+#echo $HOST unreachable
+#exit 1
+#fi
+#exit 0
+
+#echo "This script will exit with a 0 exit status"
+#echo $?
+
+cat /etc/shadow
+
+#if [ $? = "0" ]
+#then 
+#echo " Command succeded"
+#exit 0
+#else
+#echo "Command failed"
+#exit 1
+#fi
+
+
+
+
